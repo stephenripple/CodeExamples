bc9528135e65ba53009ca9ae14267d4a0a0a12c1
Stephen Ripple <stephenripple@gmail.com>
2017-08-17 17:24:27 -0400

    Solution creation and Tuple example code

---
+++ CodeExamples/Tuples.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeExamples
+{
+    public class Tuples
+    {
+        //Example instantiation
+        Tuple<int, int, string> example = new Tuple<int, int, string>(1, 1, "Test");
+
+        public static int FactoryExample()
+        {
+            Nullable<int> ret = null;
+            Task.Factory.StartNew(() =>
+            {
+                return Tuple.Create(CreateTuple());
+            }).ContinueWith(record =>
+            {
+                if (!record.IsFaulted)
+                {
+                    ret = record.Result.Item1.Item1;
+                }
+            }).Wait();
+            return (ret.HasValue ? ret.Value : 0);
+        }
+
+        private static Tuple<int, string, string, DateTime> CreateTuple()
+        {
+            Tuple<int, string, string, DateTime> ret = new Tuple<int, string, string, DateTime>(1, "Color", "Blue", DateTime.UtcNow);
+            return ret;
+        }
+    }
+}
---
+++ CodeExamplesTests/TuplesTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeExamples;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeExamples.Tests
+{
+    [TestClass()]
+    public class TuplesTests
+    {
+        [TestMethod()]
+        public void FactoryExampleTest()
+        {
+            Assert.IsTrue(CodeExamples.Tuples.FactoryExample() > 0);
+        }
+    }
+}
