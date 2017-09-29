535de8acf37b99084af4c3e4523be6b2ce0272a3
stephenripple <stephenripple@gmail.com>
2017-09-28 21:48:47 -0400

    Added Property and indexer samples

---
+++ CodeExamples/Properties.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+public class Properties
+{
+    //Basic example of using properties to provide accessibility
+    private string _privatevariable;
+    public string PublicVariable
+    {
+        get
+        {
+            return _privatevariable;
+        }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("Variable cannot be blank.", "PublicVariable");
+            _privatevariable = value;
+        }
+    }
+
+    //Build in and enforce multi threading
+    private object _syncHandle = new object();
+    private string _syncvariable;
+    public string SyncVariable
+    {
+        get
+        {
+            lock (_syncHandle)
+                return _syncvariable;
+        }
+        set
+        {
+            if (string.IsNullOrEmpty(value))
+                throw new ArgumentException("SyncVariable cannot be blank.", "SyncVariable");
+            lock (_syncHandle)
+                _syncvariable = value;
+        }
+    }
+
+    //Virtual property
+    public virtual string VirtualVariable
+    {
+        get;
+        protected set;
+    }
+
+    //Indexer 
+    private int[] _intarray = new int[] { 0, 1, 2, 3 };
+    public int this[int index]
+    {
+        get
+        {
+            return _intarray[index];
+        }
+        set
+        {
+            _intarray[index] = value;
+        }
+    }
+
+	public Properties()
+	{
+	}
+}
+
+//Virtual property on interface
+public interface IProprtyValuePair<T>
+{
+    string Variable
+    { get; }
+
+    T Value
+    { get; set; }
+}
---
+++ CodeExamplesTests/PropertiesTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CodeExamples.Tests
+{
+    [TestClass()]
+    public class PropertiesTests
+    {
+        [TestMethod()]
+        public void PropertiesExampleTest()
+        {
+            Properties _proptest = new Properties();
+            try
+            {
+                _proptest.PublicVariable = string.Empty;
+                Assert.Fail("Validation for PublicVariable has failed");
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.IsInstanceOfType(ex, typeof(ArgumentException));
+            }
+        }
+
+        [TestMethod()]
+        public void PropertiesIndexerExampleTest()
+        {
+            Properties _proptest = new Properties();
+            var result = _proptest[1];
+            Assert.IsTrue(result == 1);
+        }
+    }
+}
