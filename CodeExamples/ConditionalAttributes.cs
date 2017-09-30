a3b28cce8499624782086abfab1707c74d761294
stephenripple <stephenripple@gmail.com>
2017-09-30 18:05:28 -0400

    Added Conditional Attributes examples

---
+++ CodeExamples/ConditionalAttributes.cs
@@ -0,0 +1,65 @@
+﻿using System.Diagnostics;
+
+namespace CodeExamples
+{
+    //Condituional Attributes are a good way to build troubleshooting/debugging code into the code base but can be excluded when packaging.
+    public class ConditionalAttributes
+    {
+
+#if DEBUG
+        private string _debugmessage = string.Empty;
+        public string DebugMessage
+        {
+            get
+            {
+                Diagnostics();
+                return _debugmessage;
+            }
+            set
+            {
+                Diagnostics();
+                _debugmessage = value;
+            }
+        }
+#endif
+        private int _errornumber = 0;
+        public int ErrorNumber
+        {
+            get
+            {
+                return _errornumber;
+            }
+            set
+            {
+                _errornumber = value;
+            }
+        }
+
+        public ConditionalAttributes()
+        {
+            //I get an error instantiating the class
+            _errornumber = 1;
+        }
+
+        public int DoSomething()
+        {
+
+            return _errornumber;
+        }
+
+        [Conditional("DEBUG")]
+        private void Diagnostics()
+        {
+#if DEBUG
+            if (_errornumber > 0)
+                _debugmessage = "Exception Number: " + _errornumber.ToString();
+            else
+                _debugmessage = string.Empty;
+#endif
+            if (_errornumber > 0)
+            {
+                //Log error
+            }
+        }
+    }
+}
---
+++ CodeExamplesTests/ConditionalAttributesTests.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeExamples.Tests
+{
+    [TestClass()]
+    public class ConditionalAttributesTests
+    {
+        [TestMethod()]
+        public void ConditionalAttributesTest()
+        {
+            ConditionalAttributes ConAttrib = new ConditionalAttributes();
+#if DEBUG
+            Assert.IsFalse(string.IsNullOrEmpty(ConAttrib.DebugMessage));
+#else
+            Assert.IsTrue(true);
+#endif
+        }
+    }
+}
