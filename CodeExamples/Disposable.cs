7a41af7dfbf4bd797a14f38efdc2a7e5224635d1
stephenripple <stephenripple@gmail.com>
2017-10-05 16:34:43 -0400

    IDisposable example

---
+++ CodeExamples/Disposable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace CodeExamples
+{
+    public class Disposable : IDisposable
+    {
+        bool disposed = false;
+        public bool Disposed
+        {
+            get { return disposed; }
+        }
+
+        public SqlConnection toDisposeConnection;
+
+        public Disposable()
+        {
+            toDisposeConnection = new SqlConnection();
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+                return;
+
+            if (disposing)
+            {
+                if (toDisposeConnection != null)
+                    toDisposeConnection.Dispose();
+            }
+
+            disposed = true;
+        }
+    }
+}
---
+++ CodeExamplesTests/DisposableTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeExamples.Tests
+{
+    [TestClass()]
+    public class DisposableTests
+    {
+        [TestMethod()]
+        public void DisposeTest()
+        {
+            Disposable disposableObj;
+            //The using pattern makes sure disposable is called at the end of the code block
+            using (disposableObj = new Disposable())
+            {
+                if (disposableObj.toDisposeConnection == null)
+                {
+                    Assert.Fail();
+                }
+            }
+
+            Assert.IsTrue(disposableObj.Disposed);
+        }
+    }
+}
