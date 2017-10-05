8747197f3ce25cd84bb9f702bdc22cb965e11c73
stephenripple <stephenripple@gmail.com>
2017-10-04 22:17:50 -0400

    Added object comparison ienum

---
+++ CodeExamples/ObjectEquality.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CodeExamples
+{
+    public class ObjectEquality:IEnumerable<ObjectEquality>
+    {
+        public List<ObjectEquality> objects = null;
+        public int id = 0;
+
+        public ObjectEquality()
+        {
+            objects = new List<ObjectEquality>();
+        }
+
+        public ObjectEquality(int objectid)
+        {
+            objects = new List<ObjectEquality>();
+            id = objectid;
+        }
+
+        public override bool Equals(object toCompObj)
+        {
+            if (toCompObj.GetType() != this.GetType())
+                return false;
+
+            if (((ObjectEquality)toCompObj).id == this.id)
+                return true;
+            else
+                return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        IEnumerator<ObjectEquality> IEnumerable<ObjectEquality>.GetEnumerator()
+        {
+            return objects.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CodeExamplesTests/ObjectEqualityTests.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeExamples.Tests
+{
+    [TestClass()]
+    public class ObjectEqualityTests
+    {
+        [TestMethod()]
+        public void EqualsTestDifferent()
+        {
+            ObjectEquality obj1 = new ObjectEquality(1);
+            ObjectEquality obj2 = new ObjectEquality(2);
+
+            Assert.IsFalse(obj1.Equals(obj2));
+        }
+
+        [TestMethod()]
+        public void EqualsTestSame()
+        {
+            ObjectEquality obj1 = new ObjectEquality(1);
+            ObjectEquality obj2 = new ObjectEquality(1);
+
+            Assert.IsTrue(obj1.Equals(obj2));
+        }
+    }
+}
