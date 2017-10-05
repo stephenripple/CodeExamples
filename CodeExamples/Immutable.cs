b7211e0e38182fd4e5534938a16a6a590ca71b06
stephenripple <stephenripple@gmail.com>
2017-10-05 16:59:37 -0400

    added immutable examples

---
+++ CodeExamples/Immutable.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CodeExamples
+{
+    public class Immutable
+    {
+        public string HalfImmutable
+        {
+            get;
+            private set;
+        }
+
+        private readonly string _fullimmutable;
+        public string FullImmutable
+        {
+            get { return _fullimmutable; }
+        }
+
+        public Immutable(string fullimmutable, string halfimmutable)
+        {
+            _fullimmutable = fullimmutable;
+            HalfImmutable = halfimmutable;
+        }
+
+        //Demonstrate that this is not stricly an immutable object and can be changed internally after initialization
+        public void InternalChange(string halfimmutable)
+        {
+            HalfImmutable = halfimmutable;
+        }        
+    }
+}
---
+++ CodeExamplesTests/ImmutableTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CodeExamples.Tests
+{
+    [TestClass()]
+    public class ImmutableTests
+    {
+        [TestMethod()]
+        public void ImmutableTest()
+        {
+            Immutable mute = new Immutable("full", "half");
+            Assert.AreEqual(mute.FullImmutable, "full");
+        }
+        
+        [TestMethod()]
+        public void InternalChangeTest()
+        {
+            string changeto = "halfish";
+            Immutable mute = new Immutable("full", "half");
+            mute.InternalChange(changeto);
+
+            Assert.AreNotEqual(mute.HalfImmutable, "half");
+        }
+    }
+}
